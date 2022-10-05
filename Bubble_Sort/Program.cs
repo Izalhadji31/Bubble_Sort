10f1187998ca85a147251448ce0e51431cc824f0
Rizki Fajar <rizki.fajar.ft17@mail.umy.ac.id>
2022-10-05 22:11:41 +0700

    private int

--- Bubble_Sort/Program.cs
+++ Bubble_Sort/Program.cs
@@ -1,16 +1,16 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
 
 namespace Bubble_Sort
 {
     class Program
     {
-        static void Main(string[] args)
+        private int[] a = new int[20];
+        private int n;
+        public void read()
         {
 
         }
+        
     }
 }
