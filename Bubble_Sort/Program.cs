47d4fcfca779c8826b44a1be1dc208b990682b7c
Rizki Fajar <rizki.fajar.ft17@mail.umy.ac.id>
2022-10-05 22:31:02 +0700

    menambahkan while (true)

--- Bubble_Sort/Program.cs
+++ Bubble_Sort/Program.cs
@@ -8,11 +8,23 @@ namespace Bubble_Sort
         // Deklarasi array int dengan ukuran 20
         private int[] a = new int[20];
 
-
+        // Deklarasi variabel int umtuk menyimpan banyaknya data pada array
         private int n;
+
+        // Fungsi / Method untuk menerima masukan
         public void read()
         {
-
+            // Menerima angka untuk menentukan banyaknya data yang disimpan pada array
+            while (true)
+            {
+                Console.Write("Masukkan banyaknya elemen pada Array: ");
+                string s = Console.ReadLine();
+                n = Int32.Parse(s);
+                if (n <= 20)
+                    break;
+                else
+                    Console.WriteLine("\nArray dapat mempunyai maksimal 20 elemen.\n");
+            }
         }
         
     }
